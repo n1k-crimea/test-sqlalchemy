435a20d073a2e9d460bcccfc4a8ba977214257a4
n1k-crimea <evloshevsky@gmail.com>
2020-01-16 16:04:33 +0300

    add Database

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-Message = namedtuple('Message', 'text tag')
-messages = []
+db = SQLAlchemy(app)
 
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+    def __init__(self, text, tags):
+        self.text = text.strip()
+        self.tags = [Tag(text=tag.strip()) for tag in tags.split(',')]
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+db.create_all()
 
 @app.route('/')
 def index_page():
@@ -15,13 +30,14 @@ def index_page():
 
 @app.route('/message')
 def msg_page():
-    return render_template('message.html', messages=messages)
+    return render_template('message.html', messages=Message.query.all())
 
 @app.route('/add_message', methods=['POST'])
 def add_msg():
     text = request.form.get('text')
     tag = request.form.get('tag')
-    messages.append(Message(text, tag))
+    db.session.add(Message(text, tag))
+    db.session.commit()
     return redirect(url_for('msg_page'))
 
 if __name__ == '__main__':
